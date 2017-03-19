f1fe51e6a6cbb48142b971cf90b65fb78707d271
Tyson Gach <tyson@tysongach.com>
2017-03-19 19:34:44 -0400

    Refactor SVG helper
    Instead of hard-coding where SVG's should live, the helper now looks for
    SVG's in the images directory set by the user in the Middleman
    configuration: `:images_dir`.
    
    Closes https://github.com/thoughtbot/middleman-template/issues/3

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -17,8 +17,10 @@ module ApplicationHelpers
 
   def svg(name)
     root = Middleman::Application.root
-    file_path = "#{root}/source/images/#{name}.svg"
+    images_path = config[:images_dir]
+    file_path = "#{root}/source/#{images_path}/#{name}.svg"
+
     return File.read(file_path) if File.exists?(file_path)
-    "(not found)"
+    "(SVG not found)"
   end
 end
